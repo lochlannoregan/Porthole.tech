cf8e092213b1fe4154113cd0f05de85907538c39
lochlannoregan <mail@lochlannoregan.com>
2018-11-05 10:07:48 +0000

    Added OpenStreet Maps Test page, Updated 360 iframe to bootstrap responsive
    In this commit I added the gorumna-island.hbs file to test openstreet maps. To make this work as another page
    I added another route to the index.js file making use of the express.js library.
    I added divs around the existing 360 degree image on the main page to make it responsive and added a button
    to this new page.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/gorumna-island', function(req, res, next) {
+    res.render('gorumna-island', { title: 'Express' });
 });
 
 module.exports = router;
