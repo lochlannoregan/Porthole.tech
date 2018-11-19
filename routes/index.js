b83ee52d1a7b51d6aa09b2864bcdb9c4c05a2092
jhmu20 <38563502+jhmu20@users.noreply.github.com>
2018-11-19 17:13:21 +0000

    Added function to get random array number
    Javascript function "randomArray()" gets a random number that would be a number in the array, also added bootstrap to the button

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,10 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+function randomArray(){
+
+  var arrayNum = Math.floor((Math.random() * 5) + 1);
+
+  return arrayNum;
+}
