989afb35e814051e15c4ad0155856f039549a906
lochlannoregan <mail@lochlannoregan.com>
2018-11-26 12:46:05 +0000

    Removed unnecessary code from routes js file.
    The file was setting a title for pages which are as of yet not making use of that code. I removed it for clarity.

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.get('/gorumna-island', function(req, res, next) {
-    res.render('gorumna-island', { title: 'Express' });
+    res.render('gorumna-island');
 });
 
 module.exports = router;
