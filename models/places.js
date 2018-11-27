f489c53688c2cd7d41dd882f6f6c5175ae7c15ac
jhmu20 <38563502+jhmu20@users.noreply.github.com>
2018-11-27 15:34:37 +0000

    Broken Current Progress

---
+++ models/places.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+require('./util');
+var placesSchema = new Schema({
+    Name: {type: String},
+    Category: {type: String},
+    Length: {type: String},
+    Grade: {type: String},
+    EstimatedTime: {type: String},
+    Likes: {type: String}
+});
+module.exports = mongoose.model('Places', placesSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var Place = require('../models/places');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+/* GET place page. */
+router.get('/place', function(req, res, next) {
+    Place.find({}, function(err, places)
+    {
+        // Generate rand number between 1 and places.length
+        res.render('place', { place: places[getRandomInt(places.length)] });
+
+    });
+
+});
+
 module.exports = router;
 
-
-}
