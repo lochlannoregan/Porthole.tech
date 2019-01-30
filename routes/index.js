1d86cf91a8ffd9a3c32d309415b4090dbec87f35
jhmu20 <38563502+jhmu20@users.noreply.github.com>
2019-01-30 17:38:52 +0000

    Added some handlebars to index.js and dashboard.hbs to get all places outputted on dashboard
    Made changes so that each link (Thumbnail and Heading) goes to the corresponding page and so that each has a different url with its id added to the url

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,12 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/placePage', function (req, res, next) {
-    Place.find({}, function(err, places)
+router.get('/placePage/:id', function (req, res, next) {
+
+    Place.find({_id:req.params.id}, function(err, places)
     {
-        res.render('place', { place: places[1] });
+
+        res.render('place', { place: places[0] });
 
     });
 });
@@ -25,8 +27,13 @@ router.get('/dashboard', function(req, res, next) {
 
             Place.find({}, function(err, places)
             {
-                res.render('dashboard', { place: places[1] });
-
+                //res.render('dashboard', { place: places[1] });
+                res.render('dashboard', { place: places });
+                /*for(var i = 1; i<=places.length; i++){
+                    res.render('dashboard', { place: places[i] });
+                    $('.placeDiv').append('<img src = \'/images/national_park_{{place.NumberOfImage}}.jpg\' height="100" width="100" class="img-fluid" alt="Responsive image" >\n' +
+                        '    <h4>{{place.Name}}</h4>');
+                }*/
             });
 
 
@@ -54,6 +61,11 @@ router.get('/place', function(req, res, next) {
     });
 
 });
+
+function populatePlaces(places){
+
+
+}
 
 function verifyJwt(jwtString) {
     var value = jwt.verify(jwtString, 'CSIsTheWorst');
