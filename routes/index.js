e14fecc72ad6ecc492e01bfdefcfbe5381bee010
jhmu20 <38563502+jhmu20@users.noreply.github.com>
2019-01-31 14:59:08 +0000

    Removing some old code
    Removing old code that is not needed any more or was commented out

--- routes/index.js
+++ routes/index.js
@@ -27,17 +27,10 @@ router.get('/dashboard', function(req, res, next) {
 
             Place.find({}, function(err, places)
             {
-                //res.render('dashboard', { place: places[1] });
                 res.render('dashboard', { place: places });
-                /*for(var i = 1; i<=places.length; i++){
-                    res.render('dashboard', { place: places[i] });
-                    $('.placeDiv').append('<img src = \'/images/national_park_{{place.NumberOfImage}}.jpg\' height="100" width="100" class="img-fluid" alt="Responsive image" >\n' +
-                        '    <h4>{{place.Name}}</h4>');
-                }*/
+
             });
 
-
-            //res.render('dashboard');
         }
     }catch (err) {
         res.json({
@@ -62,16 +55,9 @@ router.get('/place', function(req, res, next) {
 
 });
 
-function populatePlaces(places){
-
-
-}
-
 function verifyJwt(jwtString) {
     var value = jwt.verify(jwtString, 'CSIsTheWorst');
     return value;
 }
-
-
 
 module.exports = router;
