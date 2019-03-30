b32beaf10ec9bde547926a57d68a3536bfedbdc0
lochlannoregan <mail@lochlannoregan.com>
2019-03-30 23:38:49 +0000

    Firebase and Well performing Vue.js Setup

--- server/models/util.js
+++
@@ -1,3 +0,0 @@
-var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://mongodb4670hj:lo0veq@danu7.it.nuigalway.ie:8717/mongodb4670', { useNewUrlParser: true });
-exports.connection = connection;
