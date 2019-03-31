0629eadfe06c21c43727585fd71eaf484cc84d97
lochlannoregan <27902576+lochlannoregan@users.noreply.github.com>
2019-03-31 22:42:16 +0100

    Dashboard Add Activity Work In Progress

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,13 @@
 <template>
-<nav>
+<div id="dashboard">
         <v-toolbar flat app>
-            <v-toolbar-side-icon class="blue--text" @click="drawer = !drawer"></v-toolbar-side-icon>
-            <v-toolbar-side-icon to="/">
-                <img src="../assets/logo.png">
-            </v-toolbar-side-icon>
+            <toolbar-side-icon>
+                <v-btn icon to="/">
+                <v-avatar>
+                    <img src="../assets/logo.png">
+                </v-avatar>
+                </v-btn>
+            </toolbar-side-icon>
             <v-toolbar-title class="blue--text">
                 <span class="font-weight-light">Porthole</span>
                 <span>.tech</span>
@@ -15,26 +18,105 @@
                 <v-icon right>exit_to_app</v-icon>
             </v-btn>
         </v-toolbar>
-    </nav>
+    
+
+    
+
+<v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+              color="pink"
+              dark
+              small
+              absolute
+              top
+              right
+              fab
+            v-on="on">
+              <v-icon>add</v-icon>
+        </v-btn>
+      </template>
+
+      <v-card>
+        <!-- <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >Add Place</v-card-title> -->
+
+        <v-card-text>
+          <v-layout row >
+        <v-flex md9>
+          <v-text-field
+            v-model="placeName"
+            label="Place Name"
+            required
+            maxlength="50"
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row >
+      <v-flex md9>
+        <v-textarea
+          name="input-7-1"
+          label="Description"
+          value=""
+          hint="This image reminds me..."
+          maxlength="750"
+          auto-grow
+          counter
+></v-textarea>
+        </v-flex>
+      </v-layout>
+
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
+            @click="dialog = false"
+          >
+            Add Place
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+</v-dialog>
+
+<!-- <AllPlaces/> -->
+
+</div>
 </template>
 
 <script>
 
 import firebase from 'firebase'
+import AllPlaces from '@/components/AllPlaces.vue'
 
 export default {
   name: 'dashboard',
+  data () {
+    return {
+      dialog: false
+    }
+  },
   methods: {
     logout: function () {
       firebase.auth().signOut().then(() => {
         this.$router.replace('login')
       })
+    },
+    addPlace: function () {
+      dialog = false
     }
   },
-  data () {
-    return {
-    }
-  }
+  components: { AllPlaces }
 }
 </script>
 
