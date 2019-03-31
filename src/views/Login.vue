f679829cd93aa488bda1f06148e66ff975da6f2b
lochlannoregan <27902576+lochlannoregan@users.noreply.github.com>
2019-03-31 19:52:50 +0100

    SSO visually in place but not fully working
    There is also a bug on chrome and edge with the icon button showing very large

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,59 @@
 <template>
-  <div class="login">
+  <v-container grid-list-xs>
     <Navbar/>
-    <h3>Sign In</h3>
-    <input type="text" v-model="email" placeholder="Email"><br>
-    <input type="password" v-model="password" placeholder="Password"><br>
-    <button @click="login">Connection</button>
-    <p>You don't have an account ? You can <router-link to="/sign-up">create one</router-link></p>
-  </div>
+    <v-form>
+      <v-layout row justify-center>
+        <v-flex xs6 md3>
+          <v-text-field
+            v-model="email"
+            label="Email"
+            required
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row justify-center>
+      <v-flex xs6 md3>
+        <v-text-field
+        v-model="password"
+        :type="'password'"
+        label="Password"
+        required></v-text-field>
+        </v-flex>
+      </v-layout>
+
+      <v-layout justify-center>
+      <v-btn
+        color="blue white--text"
+        @click="login" center>
+        Login
+      </v-btn>
+      </v-layout>
+
+      <!-- <v-layout row justify-center="">
+        <span class="font-weight-light">or Login with Google</span>
+          <button @click="socialLogin" class="social-button">
+            <img alt="Google Logo" src="../assets/google-logo.png">
+          </button>
+      </v-layout> -->
+      <v-layout justify-center row>
+        <span class="font-weight-light">or</span>
+      </v-layout>
+      
+
+<v-layout justify-center row>
+      <button @click="socialLogin" class="social-button">
+        <img alt="Google Logo" src="../assets/google-logo.png">
+      </button>
+</v-layout>
+
+      <v-layout justify-center>
+        <span class="font-weight-light">Don't have an account? You can 
+          <router-link to="sign-up">create one here</router-link></span>
+      </v-layout>
+
+    </v-form>
+   </v-container>
 </template>
 
 <script>
@@ -19,6 +66,11 @@ export default {
     return {
       email: '',
       password: ''
+      // rules: {
+      //   required: value => !!value || 'Required.',
+      //   min: v => v.length >= 8 || 'Min 8 characters',
+      //   emailMatch: () => ('The email and password you entered don\'t match')
+      // }
     }
   },
   components: { Navbar },
@@ -29,34 +81,51 @@ export default {
           this.$router.replace('dashboard')
         },
         (err) => {
-          alert('Oops. ' + err.message)
+          // this.$swal('Oops. ' + err.message)
+          this.$swal.fire({
+            type:'error',
+            title: 'Oops...',
+            text: err.message
+          })
         }
       )
-    }
+    },
+    socialLogin() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider).then((result) => {
+          this.$router.replace('dashboard');
+        }).catch((err) => {
+          this.$swal.fire({
+            type:'error',
+            title: 'Oops...',
+            text: err.message
+          })
+        })
+      }
   }
 }
 </script>
 
-<style scoped>  /* "scoped" attribute limit the CSS to this component only */
-  .login {
-    margin-top: 40px;
-  }
-  input {
-    margin: 10px 0;
-    width: 20%;
-    padding: 15px;
-  }
-  button {
+<style scoped>
+button {
     margin-top: 20px;
+    margin-bottom: 20px;
     width: 10%;
     cursor: pointer;
   }
-  p {
-    margin-top: 40px;
-    font-size: 13px;
+  .social-button {
+    width: 75px;
+    background: white;
+    padding: 10px;
+    border-radius: 100%;
+    outline: 0;
+    border: 0;
   }
-  p a {
-    text-decoration: underline;
-    cursor: pointer;
+  .social-button:active {
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.1);
+  }
+  .social-button img {
+    width: 100%;
   }
 </style>
