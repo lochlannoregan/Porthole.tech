f2f1fede97af89746dbb9516aff01b09a3786358
lochlannoregan <27902576+lochlannoregan@users.noreply.github.com>
2019-04-01 23:17:09 +0100

    Create and Read and Delete Working

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -107,7 +107,7 @@
             </v-avatar>
           </v-flex>
         </v-layout> -->
-      
+
         </v-card-text>
 
         <v-divider></v-divider>
@@ -159,9 +159,6 @@ export default {
       var user = firebase.auth().currentUser
       var userDirectory = user.email
       const db = firebase.firestore()
-          
-      // var url = valueGotten.toString()
-      console.log(this.imageLocation)
       db.collection(userDirectory).add({
         placeName: this.placeName,
         description: this.description,
@@ -169,63 +166,66 @@ export default {
         latitude: this.latitude,
         longitude: this.longitude
       })
-      .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id)
-      })
-      .catch(function(error) {
-        console.error("Error adding document: ", error)
-      })
+        .then(function (docRef) {
+          console.log('Document written with ID: ', docRef.id)
+          db.collection(userDirectory).doc(docRef.id).get().then(function (doc) {
+            const storageRef = firebase.storage()
+            storageRef.ref(userDirectory + '/' + doc.data().imageLocation).getDownloadURL().then(function (downloadURL) {
+              console.log(downloadURL)
+              db.collection(userDirectory).doc(docRef.id).update({
+                imageLocation: downloadURL
+              })
+            })
+          })
+        })
+        .catch(function (error) {
+          console.error('Error adding document: ', error)
+        })
       this.dialog = false
     },
     onChange: function (file) {
       this.imageLocation = file.name
       var user = firebase.auth().currentUser
       var userDirectory = user.email
-      if(userDirectory === null) {
+      if (userDirectory === null) {
         userDirectory = ''
       }
       // Create the file metadata
       var metadata = {
         contentType: 'image/jpeg'
-      };
+      }
 
       const storageRef = firebase.storage().ref()
       // Upload file and metadata to the object 'images/mountains.jpg'
-      var uploadTask = storageRef.child(userDirectory + '/' + file.name).put(file, metadata);
+      var uploadTask = storageRef.child(userDirectory + '/' + file.name).put(file, metadata)
 
       // Listen for state changes, errors, and completion of the upload.
       uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-        function(snapshot) {
+        function (snapshot) {
           // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-          var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log('Upload is ' + progress + '% done');
+          var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          console.log('Upload is ' + progress + '% done')
           switch (snapshot.state) {
             case firebase.storage.TaskState.PAUSED: // or 'paused'
-              console.log('Upload is paused');
-              break;
+              console.log('Upload is paused')
+              break
             case firebase.storage.TaskState.RUNNING: // or 'running'
-              console.log('Upload is running');
-              break;
+              console.log('Upload is running')
+              break
           }
-        }, function(error) {
-
-        // A full list of error codes is available at
-        // https://firebase.google.com/docs/storage/web/handle-errors
-        switch (error.code) {
-          case 'storage/unauthorized':
+        }, function (error) {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case 'storage/unauthorized':
             // User doesn't have permission to access the object
-            break;
+              break
 
-          case 'storage/canceled':
+            case 'storage/canceled':
             // User canceled the upload
-            break;
-        }}, function() {
-        // Upload completed successfully, now we can get the download URL
-        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-          // console.log(downloadURL);
-          // Dashboard.data().imageLocation = downloadURL
-        });
-      });
+              break
+          }
+        })
     }
   },
   components: { AllPlaces, 'upload-btn': UploadButton }
