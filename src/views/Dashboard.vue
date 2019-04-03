c70f4efeeb504f544200b877908cfdcfe94ad98b
lochlannoregan <27902576+lochlannoregan@users.noreply.github.com>
2019-04-03 22:18:58 +0100

    Fixed dashboard place formatting and reduced compontents to simply data sync complexities The fix for dashboard was importing the v-layout part of the vuetify library.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -111,7 +111,56 @@
       </v-card>
 </v-dialog>
 
-<AllPlaces/>
+<v-container grid-list-md fluid>
+    <v-data-iterator
+      :items="items"
+      :rows-per-page-items="rowsPerPageItems"
+      :pagination.sync="pagination"
+      no-data-text="No places have been added. Add one by clicking the plus!"
+      content-tag="v-layout"
+      row
+      wrap
+      search>
+      <template v-slot:item="props">
+        <v-flex
+          xs12
+          sm6
+          md4
+          lg3>
+          <v-card v-bind:id="props.item.identifier">
+            <v-card-title><h4>{{ props.item.placeName }}</h4></v-card-title>
+            <v-divider></v-divider>
+            <v-list>
+              <v-list-tile>
+                <v-list-tile-content>Image:</v-list-tile-content>
+                <v-list-tile-content class="align-end">
+                <v-avatar>
+                  <img v-bind:src="props.item.imageLocation">
+                </v-avatar>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile>
+                <v-list-tile-content>Description:</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{ props.item.description }}</v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile>
+                <v-btn flat color="blue light" @click="deletePlace(props.item.identifier)">
+                <v-icon right>delete</v-icon>
+                </v-btn>
+                <v-btn flat color="blue light">
+                <v-icon right>edit</v-icon>
+                </v-btn>
+                <v-btn flat color="blue light" :to="{ name: 'Place', params: { id: props.item.identifier } }">
+                <v-icon right>open_in_new</v-icon>
+                </v-btn>
+              </v-list-tile>
+            </v-list>
+          </v-card>
+          <br><br>
+        </v-flex>
+      </template>
+    </v-data-iterator>
+  </v-container>
 
 </div>
 </template>
@@ -119,7 +168,6 @@
 <script>
 
 import firebase from 'firebase/app'
-import AllPlaces from '@/components/AllPlaces.vue'
 import UserNavbar from '@/components/UserNavbar.vue'
 import UploadButton from 'vuetify-upload-button'
 
@@ -133,7 +181,12 @@ export default {
       description: '',
       imageLocation: 'testingImageLocation',
       latitude: '',
-      longitude: ''
+      longitude: '',
+      rowsPerPageItems: [4, 8, 12],
+      pagination: {
+        rowsPerPage: 4
+      },
+      items: []
     }
   },
   methods: {
@@ -215,9 +268,34 @@ export default {
               break
           }
         })
+    },
+    deletePlace: function (identifier) {
+      console.log(identifier)
+      var user = firebase.auth().currentUser
+      var userDirectory = user.email
+      const db = firebase.firestore()
+      db.collection(userDirectory).doc(identifier).delete().then(function () {
+        console.log('Document deleted successfully')
+      }).catch(function (error) {
+        console.error('Error removing documents', error)
+      })
+    },
+    loadPlaces: function () {
+      var user = firebase.auth().currentUser
+      var userDirectory = user.email
+      const db = firebase.firestore()
+      var itemsLink = this.items
+      db.collection(userDirectory).get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          itemsLink.push({ identifier: doc.id, placeName: doc.data().placeName, imageLocation: doc.data().imageLocation, description: doc.data().description })
+        })
+      })
     }
   },
-  components: { AllPlaces, 'upload-btn': UploadButton, UserNavbar }
+  mounted: function () {
+    this.loadPlaces()
+  },
+  components: { 'upload-btn': UploadButton, UserNavbar }
 }
 </script>
 
