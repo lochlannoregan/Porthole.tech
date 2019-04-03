16fdd9977efb404ffcc284c6ec3fa8d4b4c47bfa
lochlannoregan <27902576+lochlannoregan@users.noreply.github.com>
2019-04-03 14:27:32 +0100

    Can now access place data on place page

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import Home from './views/Home.vue'
 import Dashboard from './views/Dashboard.vue'
 import Login from './views/Login.vue'
 import SignUp from './views/SignUp.vue'
+import Place from './views/Place.vue'
 
 Vue.use(Router)
 
@@ -29,14 +30,6 @@ const router = new Router({
         requiresAuth: true
       }
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // },
     {
       path: '/login',
       name: 'Login',
@@ -46,14 +39,15 @@ const router = new Router({
       path: '/sign-up',
       name: 'SignUp',
       component: SignUp
+    },
+    {
+      path: '/place/:id',
+      name: 'Place',
+      component: Place,
+      meta: {
+        requiresAuth: true
+      }
     }
-    // {
-    //   path: '/home',
-    //   name: 'Home',
-    //   component: Home,
-    //   meta: {
-    //     requiresAuth: true
-    //   }
   ]
 })
 
