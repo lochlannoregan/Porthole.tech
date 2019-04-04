a6b7ab3fcebdcbdf789575b57e87c0a88f6047a5
lochlannoregan <27902576+lochlannoregan@users.noreply.github.com>
2019-04-04 12:30:11 +0100

    Final changes to views and interface

--- src/views/Place.vue
+++ src/views/Place.vue
@@ -2,23 +2,28 @@
     <div id="place">
         <UserNavbar/>
 
-        <h3>Place Name: {{ this.place[0] }} </h3>
-        <h3>Image Location: <a :href='place[1]'>See it</a></h3>
-        <h4>Description: {{ this.place[2] }} </h4>
-        <h4></h4>
-
-      <v-layout>
-        <l-map :key="markerLatLng[0]" style="height: 600px; width: 600px" :zoom="zoom" :center="this.markerLatLng">
-    <l-tile-layer :url="url"></l-tile-layer>
-    <l-marker :lat-lng="this.markerLatLng" >
-      <l-tooltip>{{this.place[0]}}</l-tooltip>
-    </l-marker>
-  </l-map>
-      <!-- <a-scene>
-        <a-sky :src='place[1]'></a-sky>
-      </a-scene> -->
-      </v-layout>
-
+        <template>
+  <v-layout justify-center>
+    <v-flex xs12 sm9>
+      <v-card>
+        <v-responsive >
+          <l-map :key="markerLatLng[0]"  style="height:800px; width: 2000px" :zoom="zoom" :center="this.markerLatLng">
+            <l-tile-layer :url="url"></l-tile-layer>
+              <l-marker :lat-lng="this.markerLatLng" >
+                <l-tooltip>{{this.place[0]}}</l-tooltip>
+             </l-marker>
+           </l-map>
+        </v-responsive>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">{{ this.place[0] }}</h3>
+            <div>{{ this.place[2] }}</div>
+          </div>
+        </v-card-title>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
     </div>
 </template>
 
@@ -35,7 +40,7 @@ export default {
       id: 0,
       place: [ ],
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-      zoom: 3,
+      zoom: 6,
       markerLatLng: [ 41.234, -1.532 ]
     }
   },
